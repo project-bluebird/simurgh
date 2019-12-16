c885c511a9e137991cf5a16dd23a2d95360aff15
Tarek Allam <t.allam.jr@gmail.com>
2019-12-16 11:06:26 +0000

    [WIP] Run single conditional check for conda env
    i.e.
    
        conda activate $ENV || conda env create -q;
    
    	modified:   install.sh

--- install.sh
+++ install.sh
@@ -3,33 +3,31 @@
 ## Set up environment and install dependecies
 
 if [ -e "environment.yml" ]; then
-  # echo "environment.yml file found"
-  # Get environment name associated with 'name' variable in file
-  ENV=`grep 'name:' environment.yml | tail -n1 | awk '{ print $2}'`
+    # echo "environment.yml file found"
+    # Get environment name associated with 'name' variable in file
+    ENV=`grep 'name:' environment.yml | tail -n1 | awk '{ print $2}'`
 
-  # Check if you are already in the environment
-  if [[ $PATH != *$ENV* ]]; then
-    # Check if the environment exists
-    conda activate $ENV
-    if [ $? -ne 0 ]; then
-      # Create the environment and activate
-      echo "Conda env '$ENV' doesn't exist."
-      echo "Creating '$ENV' environment now .. "
+  # # Check if you are already in the environment
+  # if [[ $PATH != *$ENV* ]]; then
+  #   # Check if the environment exists
+  #   conda activate $ENV
+  #   if [ $? -ne 0 ]; then
+  #     # Create the environment and activate
+  #     echo "Conda env '$ENV' doesn't exist."
+  #     echo "Creating '$ENV' environment now .. "
 
-      conda env create -q;
+  conda activate $ENV || conda env create -q;
 
-      if [ `uname` == "Linux" ]; then
-        # Export path variables for linux
-        # Resolves: 'xcb' plugin for OpenGL could not be found
-        # https://github.com/TUDelft-CNS-ATM/bluesky/wiki/Installation
-        export QT_PLUGIN_PATH="$HOME/miniconda/envs/$ENV/lib/python3.6/site-packages/PyQt5/Qt/plugins/platforms/"
-        echo $QT_PLUGIN_PATH
-      fi
-
-      conda activate $ENV;
-
-    fi
+  if [ `uname` == "Linux" ]; then
+      # Export path variables for linux
+      # Resolves: 'xcb' plugin for OpenGL could not be found
+      # https://github.com/TUDelft-CNS-ATM/bluesky/wiki/Installation
+      export QT_PLUGIN_PATH="$HOME/miniconda/envs/$ENV/lib/python3.6/site-packages/PyQt5/Qt/plugins/platforms/"
+      echo $QT_PLUGIN_PATH
   fi
+
+  conda activate $ENV;
+
 fi
 
 ## Install Bluesky
@@ -53,6 +51,3 @@ fi
 DODO_DIR=./dodo/
 
 ( cd $DODO_DIR; pip install PyDodo/ )
-
-
-
